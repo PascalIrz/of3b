1cfa62e1177d16fab11ef636d259803f7a3dbba1
PascalIrz <pascal.irz@gmail.com>
2021-08-27 15:26:27 +0200

    commenté maj_donnees_sd_atlas ()

--- R/maj_donnees_sd_atlas.R
+++ R/maj_donnees_sd_atlas.R
@@ -26,40 +26,33 @@ maj_donnees_sd_atlas <- function(
   )
 
 {
-# # Lecture du fichier shapefile à jour
-#   df_a_jour <- mef_donnees_sd_atlas(fichier_shp_a_jour = fichier_shp_a_jour)
-#
-# # lecture du fichier csv du millésime précédent
-#   sd_ancien <- read_csv2(file = fichier_csv_precedent) %>%
-#     mutate_at(vars(starts_with("date")), as.character) %>%
-#     rename(unique_obs_id_old = unique_obs_id)
 
-# assemblage des deux
   assemblage <- df_a_jour %>%
     rename(unique_obs_id_new = unique_obs_id) %>%
     left_join(df_precedent %>%
-                rename(unique_obs_id_old = unique_obs_id),
+                rename(unique_obs_id_old = unique_obs_id) %>%
+                select(-date_peche, # Champ exclu car fait capoter la jointure
+                       -date_saisie, # Champ pour éviter doublonnage
+                       -date_modif, # Champ pour éviter doublonnage
+                       -x_wgs84, # Champ qexclu car fait capoter la jointure
+                       -y_wgs84, # Champ qexclu car fait capoter la jointure
+                       -effectif), # Champ exclu au cas où la valeur aurait été mise à jour
               by = c("unique_dataset_id",
                      "unique_ope_id",
-                     "date_peche",
                      "code_exutoire",
                      "code_station",
                      "localisation",
-                     "x_wgs84",
-                     "y_wgs84",
                      "ctxte_peche",
                      "code_espece",
                      "esp_code_taxref")) %>%
     select(unique_obs_id_new,
            unique_obs_id_old,
-           everything()) %>%
-    select(-effectif.y) %>%
-    rename(effectif = effectif.x)
+           everything())
 
-# gestion des uuid. Si l'observation est nouvelle, il est créé, sinon il est conservé
-# mêrme en cas de changement de statut de présence / absence
+  # gestion des uuid. Si l'observation est nouvelle, il est créé, sinon il est conservé
+  # même en cas de changement de statut de présence / absence
   assemblage <- assemblage %>%
-    mutate(unique_obs_id = ifelse(is.na(unique_obs_id_old),
+    mutate(unique_obs_id = ifelse(test = is.na(unique_obs_id_old),
                                   yes = unique_obs_id_new,
                                   no = unique_obs_id_old)) %>%
     select(-unique_obs_id_old, -unique_obs_id_new) %>%
